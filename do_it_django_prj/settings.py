c90aa71322e8eefca9dc9d5ba5408a553e856464
Zero_Min <ymkim9510@gmail.com>
2021-11-27 23:57:51 +0900

    포스트 목록 페이지에 대표 이미지 출력

--- do_it_django_prj/settings.py
+++ do_it_django_prj/settings.py
@@ -150,6 +150,10 @@ USE_TZ = False
 
 STATIC_URL = '/static/'
 
+import os
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, '_media')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
