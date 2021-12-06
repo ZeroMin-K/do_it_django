0218b6b612131c1dc6293e5cd147a8fa4ebab6b1
Zero_Min <ymkim9510@gmail.com>
2021-12-06 23:43:13 +0900

    포스트 상세 페이지에 카테고리 기능 추가

--- blog/tests.py
+++ blog/tests.py
@@ -95,17 +95,17 @@ class TestView(TestCase):
 
     def test_post_detail(self):
         # 1.1 포스트가 하나있다.
-        post_001 = Post.objects.create(
-            title = '첫번째 포스트입니다',
-            content = 'Hello World. We are the world',
-            author = self.user_trump,
-        )
+        # post_000 = Post.objects.create(
+        #     title = '첫번째 포스트입니다',
+        #     content = 'Hello World. We are the world',
+        #     author = self.user_trump,
+        # )
         # 1.2. 그 포스트의 url은 'blog/1/'
-        self.assertEqual(post_001.get_absolute_url(), '/blog/1/')
+        self.assertEqual(self.post_001.get_absolute_url(), '/blog/1/')
 
         # 2. 첫번째 포스트의 상세 페이지 테스트
         # 2.1. 첫번째 포스트의 url로 접근시 정상적으로 작동(status code:200)
-        response = self.client.get(post_001.get_absolute_url())
+        response = self.client.get(self.post_001.get_absolute_url())
         self.assertEqual(response.status_code, 200)
         soup = BeautifulSoup(response.content, 'html.parser')
 
@@ -115,16 +115,19 @@ class TestView(TestCase):
         # self.assertIn('About Me', navbar.text)
         self.navbar_test(soup)
 
+        self.category_card_test(soup)
+
         # 2.3. 첫번째 포스트의 제목이 웹 브라우저 탭 타이틀에 출력
-        self.assertIn(post_001.title, soup.title.text)
+        self.assertIn(self.post_001.title, soup.title.text)
 
         # 2.4. 첫번째 포스트의 제목이 포스트 영역에 출력
         main_area = soup.find('div', id='main-area')
         post_area = main_area.find('div', id='post-area')
-        self.assertIn(post_001.title, post_area.text)
+        self.assertIn(self.post_001.title, post_area.text)
+        self.assertIn(self.category_programming.name, post_area.text)
 
         # 2.5. 첫번째 포스트의 작성자(author)가 포스트 영역에 출력
         self.assertIn(self.user_trump.username.upper(), post_area.text)
 
         # 2.6. 첫번째 포스트의 내용(content)이 포스트 영역에 출력
-        self.assertIn(post_001.content, post_area.text)
+        self.assertIn(self.post_001.content, post_area.text)
