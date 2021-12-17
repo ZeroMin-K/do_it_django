1448e45c952f4a72a8daf72cc6c1e8dd7d7d2668
Zero_Min <ymkim9510@gmail.com>
2021-12-17 23:51:32 +0900

    포스트 수정시 텍스트로 태그 지정

--- blog/tests.py
+++ blog/tests.py
@@ -14,7 +14,7 @@ class TestView(TestCase):
         self.category_programming = Category.objects.create(name='programming', slug='programming')
         self.category_music = Category.objects.create(name='music', slug='music')
 
-        self.tag_python_kor = Tag.objects.create(name='파이썬 공부', slug='파이썬-공부')
+        self.tag_python_kor = Tag.objects.create(name='파이썬 공부', slug='파이썬 공부')
         self.tag_python = Tag.objects.create(name='python', slug='python')
         self.tag_hello = Tag.objects.create(name='hello', slug='hello')
 
@@ -260,12 +260,17 @@ class TestView(TestCase):
         main_area = soup.find('div', id='main-area')
         self.assertIn('Edit Post', main_area.text)
 
+        tag_str_input = main_area.find('input', id='id_tags_str')
+        self.assertTrue(tag_str_input)
+        self.assertIn('파이썬 공부; python', tag_str_input.attrs['value'])
+
         response = self.client.post(
             update_post_url,
             {
                 'title': '세번째 포스트를 수정',
                 'content': '안녕 세계? 우리는 하나',
-                'category': self.category_music.pk
+                'category': self.category_music.pk,
+                'tags_str': '파이썬 공부; 한글 태그, some tag'
             },
             follow=True
         )
@@ -274,6 +279,10 @@ class TestView(TestCase):
         self.assertIn('세번째 포스트를 수정', main_area.text)
         self.assertIn('안녕 세계? 우리는 하나', main_area.text)
         self.assertIn(self.category_music.name, main_area.text)
+        self.assertIn('파이썬 공부', main_area.text)
+        self.assertIn('한글 태그', main_area.text)
+        self.assertIn('some tag', main_area.text)
+        self.assertNotIn('python', main_area.text)
 
 
 
