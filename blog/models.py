776a914f431048fed37b33779cc51222174b09bf
Zero_Min <ymkim9510@gmail.com>
2021-12-30 21:54:45 +0900

    사용자 아바타 출력

--- blog/models.py
+++ blog/models.py
@@ -73,3 +73,9 @@ class Comment(models.Model):
 
     def get_absolute_url(self):
         return f'{self.post.get_absolute_url()}#comment-{self.pk}'
+
+    def get_avatar_url(self):
+        if self.author.socialaccount_set.exists():
+            return self.author.socialaccount_set.first().get_avartar_url()
+        else:
+            return f'https://doitdjango.com/avatar/id/511/a5bf9558121fe8fe/svg/{self.author.email}'
